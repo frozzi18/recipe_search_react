9aa4f399f82fa6925d922b7dde70e30f0ff77be4
frozzi18 <42233054+frozzi18@users.noreply.github.com>
2019-07-13 17:54:51 +0200

    Update RecipeDetails.js

--- src/components/RecipeDetails.js
+++ src/components/RecipeDetails.js
@@ -6,7 +6,7 @@ export default class RecipeDetails extends Component {
         super(props)
 
         this.state = {
-            recipe:{},
+            recipe:recipe,
             url:`https://www.food2fork.com/api/get?key=2c07b23b970d91e87ac087c8053dab5&rId=${
                 this.props.id
             }`             
@@ -18,6 +18,7 @@ export default class RecipeDetails extends Component {
             image_url,
             publisher,
             publisher_url,
+            source_url,
             title,
             ingredients
         } = this.state.recipe;
@@ -29,7 +30,36 @@ export default class RecipeDetails extends Component {
                             <button className="btn btn-warning mb-5 text-capitalize">
                                 back to recipe list
                             </button>
+                            <img src={image_url} className="d-block w-100" alt="recipe" />
                         </div>
+                        {/* details */}
+                        <div className="col-10 mx-auto col-md-6 my-3">
+                            <h6 className="text-uppercase">{title}</h6>
+                            <h6 className="text-warning text-capitalize text-slanted">
+                                provided by {publisher}
+                            </h6>
+                            <a href={publisher_url} 
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="btn btn-primary mt-2 text-capitalize">publisher webpage</a>
+                            <a href={source_url} 
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="btn btn-success mt-2 mx-3 text-capitalize">recipe url</a>
+                            <ul className="list-group mt-4">
+                            <h2 className="mt-3 mb-4">Ingredients</h2>
+                            {
+                                ingredients.map((item,index) =>{
+                                    return (
+                                        <li key={index} className="list-group-item text-slanted">
+                                                {item}
+                                        </li>
+                                    )
+                                })
+                            }
+                            </ul>
+                        </div>
+
                     </div>
                 </div>
             </React.Fragment>
