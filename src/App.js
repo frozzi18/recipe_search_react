a0b9ea1bb60ec7da198c15951b40ab336c032c4c
frozzi18 <42233054+frozzi18@users.noreply.github.com>
2019-07-13 06:50:13 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import {recipes} from './tempList';
 import RecipeList from "./components/RecipeList";
 import RecipeDetails from "./components/RecipeDetails";
 
 
-function App() {
-  return (
-    <React.Fragment>
-      <RecipeList />
-      <RecipeDetails />
-    </React.Fragment>
-  );
+class App extends Component {
+  state ={
+    recipes:[],
+    url:"https://www.food2fork.com/api/search?key=2c07b23b970d91e87ac087c8053dab59"
+  };
+
+  async getRecipes(){
+    try{
+      const data = await fetch(this.state.url);
+      const jsonData =await data.json();
+      this.setState({
+        recipes:jsonData.recipes
+      })
+    } catch(error){
+      console.log(error);
+    } 
+  }
+
+  componentDidMount
+  render(){
+    return (
+      <React.Fragment>
+        <RecipeList />
+        <RecipeDetails />
+      </React.Fragment>
+    );
+  }  
 }
 
 export default App;
--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -4,7 +4,7 @@ export default class Recipe extends Component {
     render() {
         return (
             <React.Fragment>
-                Hello from recipe
+                <h1>Hello from recipe</h1>
             </React.Fragment>
         )
     }
