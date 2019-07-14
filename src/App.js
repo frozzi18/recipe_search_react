ab162562d3f08274e40a396c588c635766702d24
frozzi18 <42233054+frozzi18@users.noreply.github.com>
2019-07-14 14:00:56 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -40,9 +40,9 @@ class App extends Component {
     } 
   }
 
-  // componentDidMount(){
-  //   this.getRecipes()
-  // }
+  componentDidMount(){
+    this.getRecipes()
+  }
 
   displayPage = (index) => {
     switch(index){
